510a3b745464e70f6092f91487282149434dddbf
can-sari04 <80253747+can-sari04@users.noreply.github.com>
2024-01-22 22:33:43 +0300

    Add files via upload

---
+++ cipherEncoder.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+//by Can Sarý , Batuhan Arda Bekar
+int main()
+{
+    int a, b; // f(p) = a.p + b
+
+    cout << "Enter a [ f(p) = a.p + b ] :";
+    cin >> a;
+    cout << "Enter b [ f(p) = a.p + b ] :";
+    cin >> b;
+
+    string text;
+    cout << "Enter Text: ";
+    cin >> text;
+
+    cout << '\n';
+    char alphabetIndex[26];
+    char cipherIndex[26];
+
+    for (int i = 65; i < 91; i++) {
+
+        alphabetIndex[i - 65] = i;
+        int temp = alphabetIndex[i - 65] - 65;
+        cipherIndex[i - 65] = (temp * a + b) % 26 + 65;
+    }
+
+    for (int i = 0; i < text.size(); i++)
+    {
+        text[i] = toupper(text[i]);
+        int temp = text[i] - 65;
+        for (int j = 0; j < 26; j++) {
+            if (temp == j)
+                cout << cipherIndex[j];
+
+        }
+    }
+}
